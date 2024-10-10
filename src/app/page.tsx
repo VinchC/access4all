e49fc466e178eed30876918b840da19651938b30
Vincent C <vinch.cm@gmail.com>
2024-10-10 17:33:04 +0200

    Head and Header to review, CSS over Bootstrap and skip navigation link to implement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,43 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <Header />
+      {/* destination of the skip navigation link */}
+    <main id="maincontent">
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <h1 id="parent">To do list</h1>
+      <div className="form-check form-switch">
+        <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Skip navigation link</label>
+      </div>
+
+
+      <ul>
+        <li>Checkbox for tasks done</li>
+        <li>Change the size of the element</li>
+        <li>Slider</li>
+        <li>Day / night mode</li>
+        <li>SearchBar</li>
+        <li>Card that will display content on user action</li>
+        <li>Install the package Pa11y</li>
+        <li>Contact form</li>
+        <li>Signin form</li>
+        <li>Agenda?</li>
+      </ul>
+
+      <h2></h2>
+
+      <h3></h3>
+
+      <section>
+        <article>
+          <h4></h4>
+          <p></p>
+        </article>
+      </section>
+    </main>
+    </>
   );
 }
---
+++ src/components/Head.tsx
@@ -0,0 +1,22 @@
+import "../app/globals.css";
+
+export default function Head() {
+  return (
+    <>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="A page to work on good practices regarding the topic of web accessibility."
+        />
+        <title>Accessibility 101</title>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossOrigin="anonymous"
+        />
+        <link rel="stylesheet" href="../app/globals.css" />
+    </>
+  );
+}
